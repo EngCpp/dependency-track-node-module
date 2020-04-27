7b1d9078d87a4a9d670571db52f64ea89c1d8c77
engcpp <engcpp@yahoo.com.br>
2020-04-27 18:49:23 +1200

    Small refactoring

--- utils/misc-utils.js
+++ utils/misc-utils.js
@@ -1,3 +1,5 @@
+const {isEmpty} = require("./string-utils");
+const {config} = require("../config");
 
 sleep = async(ms) => {
   return new Promise(resolve => setTimeout(resolve, ms));
@@ -14,4 +16,49 @@ exports.showProgressBarAnimation = async(label, millieSeconds) => {
       console.clear();
       console.log(label + '|' + ('█'.repeat(i)) + ('-'.repeat(iterations-i)) + '|' + ((100*i)/iterations) + '%');
     }
+}
+
+/**
+* Match a given textKey with an array of argument
+*/
+exports.loadConfig = (textKey, arguments) => {
+  let keyIndex = arguments.indexOf(textKey);
+  if (keyIndex > -1) {
+     return arguments[keyIndex + 1];
+  }
+
+  return null;
+}
+
+/**
+* Abstraction to enable/match action argument with a callback function
+*/
+exports.ActionMenu = function(args) {
+  this.arguments = args;
+  this.selectedArg = null;
+
+  this.when = function(actionArg, callback) {
+    const index = this.arguments.indexOf(actionArg);
+
+    if (index > -1 && callback) {
+        this.selectedArg = actionArg;
+        callback(this,index).catch(e => {
+          const {failOnError} = config;
+          console.log(e.message);
+          if (failOnError) {
+            console.log("Scripts will be terminated");
+            process.exit(1);
+          }
+        });
+    }
+
+    return this;
+  }
+
+  this.help = function(callback) {
+    if (callback && isEmpty(this.selectedArg)) {
+      callback(this);
+    }
+    return this;
+  }
 }
